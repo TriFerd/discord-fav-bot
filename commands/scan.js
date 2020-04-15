57c6a350c4dcc63de3488ebfccf7e67ba33c9a9b
TriFerd <TriFerds@freenet.de>
2020-04-15 20:53:50 +0200

    Adds basic limit workaround method

--- commands/scan.js
+++ commands/scan.js
@@ -12,6 +12,14 @@ module.exports = {
       return;
     }
 
+    // prepare args
+    const parameters = args.filter(arg => arg.startsWith('--')).map(arg => arg.substring(2));
+    let doHardScan = false;
+    if (parameters.includes('hard')) {
+      doHardScan = true;
+    }
+    const limit = args.map(arg => parseInt(arg)).filter(arg => !isNaN(arg))[0];
+
     // init database for dest channel
     const guildId = message.guild.id;
     const keyv = getKeyvForGuild(guildId);
@@ -24,40 +32,68 @@ module.exports = {
         }
         const destChannel = client.channels.cache.get(destChannelId);
 
-        // fetch last 100 messages
-        message.channel.messages.fetch({ limit: 100 })
-          .then(messages => {
-            const messageArray = [];
-            for (const [, m] of messages) {
-              // check for reactions
-              const reactionCache = m.reactions.cache;
-              if (reactionCache.has(checkedReaction)) {
-                break;
-              } else if (reactionCache.has(favReaction)) {
-                // collect valid messages
-                if (m.attachments.size) {
-                  const messageAttachmentUrl = m.attachments.values().next().value.url;
-                  messageArray.push({ content: m.content, attachmentUrl: messageAttachmentUrl });
-                } else {
-                  messageArray.push({ content: m.content, attachmentUrl: '' });
-                }
-                m.react(checkedReaction);
-              }
-            }
+        // todo: add method for fetching more than 100 messages int othe function itself, otherwise the whole scan will proceed either way (even if scanning a already scanned message)
 
-            // send messages in reverse order of scanning
-            messageArray.reverse();
-            for (const el of messageArray) {
-              if (el.attachmentUrl !== '') {
-                // send attachment
-                destChannel.send(el.content, { files: [el.attachmentUrl] });
+        message.channel.send('Scanning...');
+        // fetch last messages
+        getUnlimitedMessagesFromChannel(message.channel, limit || 1000).then(rawMessages => {
+          const messageArray = [];
+          for (const m of rawMessages) {
+            // check for reactions
+            const reactionCache = m.reactions.cache;
+            if (reactionCache.has(checkedReaction) && !doHardScan) {
+              break;
+            } else if (reactionCache.has(favReaction) && !reactionCache.has(checkedReaction)) {
+              // collect valid messages
+              if (m.attachments.size) {
+                const messageAttachmentUrl = m.attachments.values().next().value.url;
+                messageArray.push({ content: m.content, attachmentUrl: messageAttachmentUrl });
               } else {
-                if (el.content !== '') {
-                  destChannel.send(el.content);
-                }
+                messageArray.push({ content: m.content, attachmentUrl: '' });
+              }
+              m.react(checkedReaction);
+            }
+          }
+
+          // send messages in reverse order of scanning
+          messageArray.reverse();
+          const promises = [];
+          for (const el of messageArray) {
+            if (el.attachmentUrl !== '') {
+              // send attachment
+              promises.push(destChannel.send(el.content, { files: [el.attachmentUrl] }));
+            } else {
+              if (el.content !== '') {
+                promises.push(destChannel.send(el.content));
               }
             }
-          }).catch(console.error);
-      });
+          }
+          Promise.all(promises).then(x => {
+            message.channel.send('Scanning done!');
+          });
+        });
+      }).catch(console.error);
   }
 };
+
+async function getUnlimitedMessagesFromChannel (channel, limit) {
+  const allMessages = [];
+  let lastMessageId;
+
+  while (true) {
+    const options = { limit: 100 }; // discord only allows a limit of 100 at once
+    if (lastMessageId) {
+      options.before = lastMessageId;
+    }
+
+    const messages = await channel.messages.fetch(options);
+    allMessages.push(...messages.array());
+    lastMessageId = messages.last().id;
+
+    if (messages.size !== 100 || allMessages >= limit) {
+      break;
+    }
+  }
+
+  return allMessages;
+}
